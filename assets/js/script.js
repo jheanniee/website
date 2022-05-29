1ea1df24bee0ee79ed0bd93b4f8b96c3e7722f09
jheanniee <71245287+jheanniee@users.noreply.github.com>
2022-05-29 21:31:01 +0800

    revisions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,6 @@ if(navClose){
 }
 const navLink = document.querySelectorAll('.nav__link')
 
-
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
@@ -40,3 +39,22 @@ let swiper = new Swiper(".courses__container", {
         rotate: 0,
     },
 })
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
