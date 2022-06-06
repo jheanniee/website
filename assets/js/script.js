4c1ea143112e4e2b906439bc51bd9fe69217ecde
jheanniee <71245287+jheanniee@users.noreply.github.com>
2022-06-06 21:45:22 +0800

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,6 +40,7 @@ let swiper = new Swiper(".courses__container", {
     },
 })
 
+
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -58,3 +59,41 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
